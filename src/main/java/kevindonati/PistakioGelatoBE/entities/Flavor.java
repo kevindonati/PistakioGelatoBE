49f519d0d988ea0c1bb4610e5b407c4dcb753c5c
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-28 16:06:25 +0200

    flavor entity done

---
+++ src/main/java/kevindonati/PistakioGelatoBE/entities/Flavor.java
@@ -0,0 +1,49 @@
+package kevindonati.PistakioGelatoBE.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "flavors")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Flavor {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false, unique = true, name = "referral_code")
+    private String referralCode;
+
+    private String image;
+
+    @Column(nullable = false, name = "stock_portions")
+    private int stockPortions;
+
+    @Column(nullable = false)
+    private boolean available;
+
+    @Column(nullable = false)
+    private boolean vegan;
+
+    @Column(nullable = false, name = "lactose_free")
+    private boolean lactoseFree;
+
+    @Column(nullable = false, name = "gluten_free")
+    private boolean glutenFree;
+
+    @Column(nullable = false, name = "sugar_free")
+    private boolean sugarFree;
+}
