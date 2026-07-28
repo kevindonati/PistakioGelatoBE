082de9e7fbe356571d4d5fe5ab834f5909e0ab83
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-28 12:04:46 +0200

    Initial commit

---
+++ src/main/java/kevindonati/PistakioGelatoBE/PistakioGelatoBeApplication.java
@@ -0,0 +1,13 @@
+package kevindonati.PistakioGelatoBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PistakioGelatoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PistakioGelatoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kevindonati/PistakioGelatoBE/PistakioGelatoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kevindonati.PistakioGelatoBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PistakioGelatoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
