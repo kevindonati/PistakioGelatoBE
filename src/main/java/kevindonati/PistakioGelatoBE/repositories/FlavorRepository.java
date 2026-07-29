d56e0725be0491640b7b1a130a34b4fbf17bbaba
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-29 14:04:13 +0200

    flavor repository done

---
+++ src/main/java/kevindonati/PistakioGelatoBE/repositories/FlavorRepository.java
@@ -0,0 +1,23 @@
+package kevindonati.PistakioGelatoBE.repositories;
+
+import kevindonati.PistakioGelatoBE.entities.Flavor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface FlavorRepository extends JpaRepository<Flavor, UUID> {
+    List<Flavor> findByAvailableTrue();
+
+    List<Flavor> findByCategoryId(UUID categoryId);
+
+    List<Flavor> findByVeganTrue();
+
+    List<Flavor> findByGlutenFreeTrue();
+
+    List<Flavor> findByLactoseFreeTrue();
+
+    List<Flavor> findBySugarFreeTrue();
+}
