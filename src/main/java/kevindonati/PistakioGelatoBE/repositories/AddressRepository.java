cf926f9e687c18f66e556db5eb80aa1026ec228a
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-29 13:46:58 +0200

    address repository done

---
+++ src/main/java/kevindonati/PistakioGelatoBE/repositories/AddressRepository.java
@@ -0,0 +1,11 @@
+package kevindonati.PistakioGelatoBE.repositories;
+
+import kevindonati.PistakioGelatoBE.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
