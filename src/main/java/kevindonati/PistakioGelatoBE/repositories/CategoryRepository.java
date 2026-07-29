2b7bc102442340363e21378ad69cc7ae68929ebf
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-29 14:03:01 +0200

    Update CategoryRepository.java

--- src/main/java/kevindonati/PistakioGelatoBE/repositories/CategoryRepository.java
+++ src/main/java/kevindonati/PistakioGelatoBE/repositories/CategoryRepository.java
@@ -2,6 +2,7 @@ package kevindonati.PistakioGelatoBE.repositories;
 
 import kevindonati.PistakioGelatoBE.entities.Category;
 import kevindonati.PistakioGelatoBE.entities.Flavor;
+import org.hibernate.internal.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,15 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
-    List<Flavor> findByAvailableTrue();
+    Optional<Category> findByName(String name);
 
-    List<Flavor> findByCategoryId(UUID categoryId);
-
-    List<Flavor> findByVeganTrue();
-
-    List<Flavor> findByGlutenFreeTrue();
-
-    List<Flavor> findByLactoseFreeTrue();
-
-    List<Flavor> findBySugarFreeTrue();
+    boolean existsByName(String name);
 }
