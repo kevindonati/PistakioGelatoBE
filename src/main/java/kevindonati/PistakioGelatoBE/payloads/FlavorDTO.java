3c23ee9311fd21432761d3f97e78090db09959f4
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-30 09:58:52 +0200

    flavorDTO done

---
+++ src/main/java/kevindonati/PistakioGelatoBE/payloads/FlavorDTO.java
@@ -0,0 +1,37 @@
+package kevindonati.PistakioGelatoBE.payloads;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record FlavorDTO(
+        @NotBlank(message = "The name of the flavor is mandatory")
+        String name,
+
+        String description,
+
+        @NotBlank(message = "The referral  code for the flavor is mandatory")
+        String referralCode,
+
+        String image,
+
+        @NotBlank(message = "The stock quantity for the flavor is mandatory")
+        @Min(value = 0, message = "The stoch can't be lower than 0")
+        int stockPortions,
+
+        boolean available,
+
+        boolean vegan,
+
+        boolean lactoseFree,
+
+        boolean glutenFree,
+
+        boolean sugarFree,
+
+        @NotNull(message = "The category is mandatory")
+        UUID category
+) {
+}
