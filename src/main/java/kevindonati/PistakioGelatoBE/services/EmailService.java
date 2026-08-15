d07091b8f9379a591502478f1715196f49bf808b
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-08-15 14:52:57 +0200

    implemented 4 language for welcome email

--- src/main/java/kevindonati/PistakioGelatoBE/services/EmailService.java
+++ src/main/java/kevindonati/PistakioGelatoBE/services/EmailService.java
@@ -3,28 +3,36 @@ package kevindonati.PistakioGelatoBE.services;
 import kevindonati.PistakioGelatoBE.entities.Order;
 import kevindonati.PistakioGelatoBE.entities.Shipment;
 import kevindonati.PistakioGelatoBE.entities.User;
+import kevindonati.PistakioGelatoBE.enums.Language;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private MessageSource messageSource;
+
+    private String getMessage(String key, Language language, Object... args) {
+        Locale locale = Locale.forLanguageTag(language.name());
+
+        return messageSource.getMessage(key, args, locale);
+    }
+
     public void sendWelcomeEmail(User user) {
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setFrom("kevindonati5@gmail.com");
         message.setTo(user.getEmail());
-        message.setSubject("Benvenuto su PistakioGelato");
-        message.setText(
-                "Ciao " + user.getName() + "!\n\n" +
-                        "Benvenuto su PistakioGelato \n\n" +
-                        "Il tuo account è stato creato con successo.\n\n" +
-                        "Buon gelato!"
-        );
+        message.setSubject(getMessage("welcome.subject", user.getLanguage()));
+        message.setText(getMessage("welcome.text", user.getLanguage(), user.getName()));
         mailSender.send(message);
     }
 
