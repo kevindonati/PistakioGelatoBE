223d09537ae2e304b6d5cbb0b612166214814fa6
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-09-04 18:50:02 +0200

    Update SecurityConfig.java

--- src/main/java/kevindonati/PistakioGelatoBE/security/SecurityConfig.java
+++ src/main/java/kevindonati/PistakioGelatoBE/security/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
         corsConfiguration.setAllowedOrigins(
                 List.of("http://localhost:5173",
                         "http://172.20.10.2:5173",
-                        "https://pistakiogelatobe-production.up.railway.app")
+                        "https://pistakio-gelato-fe-eta.vercel.app/")
         );
         corsConfiguration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
